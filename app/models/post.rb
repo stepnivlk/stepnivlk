4e102e4f963fc304887936543b3ef23a877eee4c
Tomas Koutsky <tomas@stepnivlk.net>
2015-09-23 07:55:39 +0200

    another styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  def content_before_break
+    content.partition(/\*{3}/)[0]
+  end
 end
