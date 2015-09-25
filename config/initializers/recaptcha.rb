5708abaced3c4cccacaebfe69d2a8f60245910dd
Tomas Koutsky <tomas@stepnivlk.net>
2015-09-25 14:09:55 +0200

    recaptcha fix

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LfBiQ0TAAAAAAHF7OpkzvqpsaG0we-A-QuNJwfZ'
-  config.private_key = '6LfBiQ0TAAAAAOPo6nBeRYtxXWWlW6264IKN45eE'
+  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
