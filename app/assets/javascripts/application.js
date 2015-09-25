eb24445212dc8dc8f5b2d762ff18e476b535ee01
Tomas Koutsky <tomas@stepnivlk.net>
2015-09-25 14:04:54 +0200

    recaptcha fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,41 +17,36 @@
 //= require_tree .
 $(document).ready(function() {
 
+  $('a[href*=#]').bind("click", function(e){          
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+  });
 
-        $('a[href*=#]').bind("click", function(e){
-           
-      var anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: $(anchor.attr('href')).offset().top
-      }, 1000);
-      e.preventDefault();
-    });
-
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('.scroll-up').fadeIn();
-      } else {
-        $('.scroll-up').fadeOut();
-      }
-    });
-
-      $('.header').sticky({
-      topSpacing: 0
-    });
-
-
-      $(".screen-height").height($(window).height());
-
-    $(window).resize(function(){
-      $(".screen-height").height($(window).height());
-    });
-
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-      $('#home').css({'background-attachment': 'scroll'});
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-up').fadeIn();
     } else {
-      $('#home').parallax('50%', 0.1);
+      $('.scroll-up').fadeOut();
     }
-    
+  });
 
+  $('.header').sticky({
+    topSpacing: 0
+  });
+
+  $(".screen-height").height($(window).height());
+
+  $(window).resize(function(){
+    $(".screen-height").height($(window).height());
+  });
+
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    $('#home').css({'background-attachment': 'scroll'});
+  } else {
+    $('#home').parallax('50%', 0.1);
+  }  
 
 });
